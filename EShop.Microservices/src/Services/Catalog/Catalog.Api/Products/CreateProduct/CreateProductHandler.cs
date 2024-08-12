79ec6a4c362673b71f281281fbf36deb4673a9f1
Davood Hanifi <davood.hanifi@gmail.com>
2024-08-12 23:29:13 +0200

    Added Buildng Blockboxes

---
+++ EShop.Microservices/src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,18 @@
+﻿using BuildingBlocks.CQRS;
+using MediatR;
+using System.Windows.Input;
+
+namespace Catalog.Api.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Categories, string Description, string ImageFile, decimal Price)
+    : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+public class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
