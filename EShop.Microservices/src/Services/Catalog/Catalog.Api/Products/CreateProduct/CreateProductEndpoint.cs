a81ff01df8e237cf054ade46906a60dc25070d6a
Davood Hanifi <davood.hanifi@gmail.com>
2024-08-13 23:34:28 +0200

    Added Create Product endpoint.

--- EShop.Microservices/src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductEndpoint.cs
+++ EShop.Microservices/src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,11 +1,24 @@
-﻿using Carter;
-
+﻿
 namespace Catalog.Api.Products.CreateProduct;
 
 public class CreateProductEndpoint : ICarterModule
 {
+    public record CreateProductRequest(string Name, List<string> Categories, string Description, string ImageFile, decimal Price);
+    public record CreateProductResponse(Guid Id);
+
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        throw new NotImplementedException();
+        app.MapPost("/products", async (CreateProductRequest request, ISender mediator) =>
+        {
+            var command = new CreateProductCommand(request.Name, request.Categories, request.Description, request.ImageFile, request.Price);
+            var result = await mediator.Send(command);
+            var response = new CreateProductResponse(result.Id);
+            return Results.Created($"/products/{response.Id}", response);
+        })
+        .WithName("CreateProduct")
+        .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Create Product")
+        .WithDescription("Create Product");
     }
 }
--- EShop.Microservices/src/Services/Catalog/Catalog.Api/Program.cs
+++ EShop.Microservices/src/Services/Catalog/Catalog.Api/Program.cs
@@ -1,10 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
+builder.Services.AddCarter();
+
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+});
+
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
 
-app.MapGet("/", () => "Hello World!");
-
+app.MapCarter();
 app.Run();
