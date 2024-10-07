87d44f7af37475536da076e2cd77f9d4aefcdd3e
Davood Hanifi <davood.hanifi@gmail.com>
2024-10-07 18:43:22 +0200

    Added Get product endpoint.

---
+++ EShop.Microservices/src/Services/Catalog/Catalog.Api/Products/GetProduct/GetProductEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml.Linq;
+
+namespace Catalog.Api.Products.CreateProduct;
+
+public class GetProductEndpoint : ICarterModule
+{
+    public record GetProductRequest(Guid Id);
+    public record GetProductResponse(Product Product);
+
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/{id}", async (Guid id, ISender mediator) =>
+        {
+            var result = await mediator.Send(new GetProductQuery(id));
+            var response = result.Adapt<GetProductResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("GetProduct")
+        .Produces<GetProductResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Get Product")
+        .WithDescription("Get Product");
+    }
+}
---
+++ EShop.Microservices/src/Services/Catalog/Catalog.Api/Products/GetProduct/GetProductHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace Catalog.Api.Products.CreateProduct;
+
+public record GetProductQuery(Guid Id) : IQuery<GetProductResult>;
+
+public record GetProductResult(Product Product);
+    
+public class GetProductQueryHandler(IDocumentSession session) : IQueryHandler<GetProductQuery, GetProductResult>
+{
+    private readonly IDocumentSession _session = session;
+
+    public async Task<GetProductResult> Handle(GetProductQuery query, CancellationToken cancellationToken)
+    {
+        var result = await _session.LoadAsync<Product>(query.Id, cancellationToken);
+        
+        return new GetProductResult(result);
+    }
+}
