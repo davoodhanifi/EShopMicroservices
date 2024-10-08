f91ae942cf309d28341db17435b3cadf1267c45a
Davood Hanifi <davood.hanifi@gmail.com>
2024-10-08 22:35:23 +0200

    Added Get Product By Category

---
+++ EShop.Microservices/src/Services/Catalog/Catalog.Api/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace Catalog.Api.Products.GetProductByCategory;
+
+public record GetProductByCategoryQuery(string Category): IQuery<GetProductByCategoryResult>;
+public record GetProductByCategoryResult(IEnumerable<Product> Products);
+
+public class GetProductByCategoryHandler (IDocumentSession session, ILogger<GetProductByCategoryHandler> logger) : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+{
+    private readonly IDocumentSession _session = session;
+    private readonly ILogger<GetProductByCategoryHandler> _logger = logger;
+
+    public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation($"GetProductByCategoryHandler.Handle called with query: {query}");
+        var products = await _session.Query<Product>()
+                                   .Where(p => p.Categories.Contains(query.Category))
+                                   .ToListAsync(cancellationToken);
+
+        return new GetProductByCategoryResult(products);
+    }
+}
+
