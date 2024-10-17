7c31bee6f2821a0fa2b2bc0ccce77468c2662b7c
Davood Hanifi <davood.hanifi@gmail.com>
2024-10-17 21:53:09 +0200

    Added ValidationBehavior middleware.

---
+++ EShop.Microservices/src/BuildingBlocks/BuildingBlocks/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using BuildingBlocks.CQRS;
+using FluentValidation;
+
+namespace BuildingBlocks.Behaviors;
+
+public class ValidationBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators) 
+                                                     : IPipelineBehavior<TRequest, TResponse>
+                                                       where TRequest : ICommand<TResponse>
+{
+    private readonly IEnumerable<IValidator<TRequest>> _validators = validators;
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var context = new ValidationContext<TRequest>(request);
+        var result = await Task.WhenAll(_validators.Select(x=> x.ValidateAsync(context, cancellationToken)));
+
+        var failures = result.Where(x => x.Errors.Any())
+                            .SelectMany(x => x.Errors)
+                            .ToList();
+
+        if (failures.Count > 0)
+        {
+            throw new ValidationException(failures);
+        }
+
+        return await next();
+    }
+}
--- EShop.Microservices/src/Services/Catalog/Catalog.Api/Program.cs
+++ EShop.Microservices/src/Services/Catalog/Catalog.Api/Program.cs
@@ -1,13 +1,19 @@
+using BuildingBlocks.Behaviors;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
-builder.Services.AddCarter();
 
 var assembly = typeof(Program).Assembly;
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
 });
+
+builder.Services.AddValidatorsFromAssembly(assembly);
+
+builder.Services.AddCarter();
 
 builder.Services.AddMarten(opts =>
 {
