f0f81e37e0e70e7028a456df0822b4186ade5c52
Davood Hanifi <davood.hanifi@gmail.com>
2024-10-17 21:53:38 +0200

    Added CreateProductValidator.

--- EShop.Microservices/src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
+++ EShop.Microservices/src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
@@ -5,6 +5,18 @@ public record CreateProductCommand(string Name, List<string> Categories, string 
 
 public record CreateProductResult(Guid Id);
 
+public class CreateProductValidator : AbstractValidator<CreateProductCommand>
+{
+    public CreateProductValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required!");
+        RuleFor(x => x.Categories).NotEmpty().WithMessage("Categories is required!");
+        RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required!");
+        RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required!");
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than zero!");
+    }
+}
+
 public class CreateProductCommandHandler(IDocumentSession session, ILogger<CreateProductCommandHandler> logger) : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     private readonly IDocumentSession _session = session;
