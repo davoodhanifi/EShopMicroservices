ff3c3b514437c693ff2ea9e900cbbb395262c7de
Davood Hanifi <davood.hanifi@gmail.com>
2024-11-06 23:31:10 +0100

    Added GetBasket endpoint and handler.

---
+++ EShop.Microservices/src/Services/Basket/Basket.Api/Models/ShoppingCart.cs
@@ -0,0 +1,18 @@
+﻿namespace Basket.Api.Models;
+
+public class ShoppingCart
+{
+    public string UserName { get; set; } = default!;
+    public List<ShoppingCartItem> Items { get; set; } = [];
+    public decimal TotalPrice => Items.Sum(i => i.Quantity * i.Price);
+
+    public ShoppingCart(string userName)
+    {
+        UserName = userName;
+    }
+
+    // Required for mapping
+    public ShoppingCart()
+    {
+    }
+}
