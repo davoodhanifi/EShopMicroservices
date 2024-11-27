01dc98700a814d7554e35f9900ae56750cb735b3
Davood Hanifi <davood.hanifi@greenflux.eu>
2024-11-27 23:53:13 +0100

    Added Discount.Grpc

--- EShop.Microservices/EShop.Microservices.AppHost/Program.cs
+++ EShop.Microservices/EShop.Microservices.AppHost/Program.cs
@@ -4,4 +4,6 @@ builder.AddProject<Projects.Catalog_Api>("catalog-api");
 
 builder.AddProject<Projects.Basket_Api>("basket-api");
 
+builder.AddProject<Projects.Discount_Grpc>("discount-grpc");
+
 builder.Build().Run();
