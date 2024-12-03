794443ffb6ac28633e2c4c4d7e4c967685b6efc6
Davood Hanifi <davood.hanifi@gmail.com>
2024-12-03 21:54:14 +0100

    Added Seed data and migration extention.

--- EShop.Microservices/src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
+++ EShop.Microservices/src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Discount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Discount.Grpc.Data;
 
@@ -8,5 +9,13 @@ public class DiscountContext :DbContext
 
     public DiscountContext(DbContextOptions<DiscountContext> options) : base(options)
     {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Coupon>().HasData(
+            new Coupon { Id = 1, ProductName = "IPhone X", Description = "IPhone Discount", Amount = 150 },
+            new Coupon { Id = 2, ProductName = "Samsung 10", Description = "Samsung Discount", Amount = 100 }
+            );
     }
 }
