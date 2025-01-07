6d2e67642bbabffca5e1de62d89d8072ccba568d
Davood Hanifi <davood.hanifi@gmail.com>
2025-01-07 17:31:05 +0100

    Adder Ordering.Api project

--- EShop.Microservices/EShop.Microservices.AppHost/Program.cs
+++ EShop.Microservices/EShop.Microservices.AppHost/Program.cs
@@ -6,4 +6,6 @@ builder.AddProject<Projects.Basket_Api>("basket-api");
 
 builder.AddProject<Projects.Discount_Grpc>("discount-grpc");
 
+builder.AddProject<Projects.Ordering_Api>("ordering-api");
+
 builder.Build().Run();
