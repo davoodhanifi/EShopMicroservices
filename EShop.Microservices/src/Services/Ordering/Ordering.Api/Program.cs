bc90a546cf35bce420b519560ed2a8137c949fe6
Davood Hanifi <davood.hanifi@gmail.com>
2025-03-17 16:57:40 +0100

    Added seeding data for ORdering db.

--- EShop.Microservices/src/Services/Ordering/Ordering.Api/Program.cs
+++ EShop.Microservices/src/Services/Ordering/Ordering.Api/Program.cs
@@ -1,5 +1,6 @@
 using Ordering.Api.Extensions;
 using Ordering.Application.Extensions;
+using Ordering.Infrastructure.Data.Extensions;
 using Ordering.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,9 @@ var app = builder.Build();
 
 app.UseApiServices();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    await app.InitialiseDatabaseAsync();
+}
 
 app.Run();
---
+++ EShop.Microservices/src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtentions.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastructure.Data.Extensions;
+
+public static class DatabaseExtentions
+{
+    public static async Task InitialiseDatabaseAsync(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        dbContext.Database.MigrateAsync().GetAwaiter().GetResult();
+
+        await SeedDataAsync(dbContext);
+    }
+
+    private static async Task SeedDataAsync(ApplicationDbContext dbContext)
+    {
+        await SeedCustomersAsync(dbContext);
+        await SeedProductsAsync(dbContext);
+        await SeedOrdersAsync(dbContext);
+    }
+
+    private static async Task SeedCustomersAsync(ApplicationDbContext dbContext)
+    {
+        if(!await dbContext.Customers.AnyAsync())
+        {
+            await dbContext.Customers.AddRangeAsync(InitialData.Customers);
+            await dbContext.SaveChangesAsync();
+        }
+    }
+
+    private static async Task SeedProductsAsync(ApplicationDbContext dbContext)
+    {
+        if (!await dbContext.Products.AnyAsync())
+        {
+            await dbContext.Products.AddRangeAsync(InitialData.Products);
+            await dbContext.SaveChangesAsync();
+        }
+    }
+
+    private static async Task SeedOrdersAsync(ApplicationDbContext dbContext)
+    {
+        if (!await dbContext.Orders.AnyAsync())
+        {
+            await dbContext.Orders.AddRangeAsync(InitialData.OrdersWithItems);
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
