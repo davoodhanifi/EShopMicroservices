1777095a5b9fb48eb5132336b0ae93392403db2f
Davood Hanifi <davood.hanifi@gmail.com>
2025-03-23 14:15:20 +0100

    Added Domain events dispatch interceptor.

---
+++ EShop.Microservices/src/Services/Ordering/Ordering.Infrastructure/Data/Interceptors/DispatchDomainEventsInterceptor.cs
@@ -0,0 +1,46 @@
+﻿
+using MediatR;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Ordering.Domain.Abstractions;
+
+namespace Ordering.Infrastructure.Data.Interceptors;
+
+public class DispatchDomainEventsInterceptor(IMediator mediator) : SaveChangesInterceptor
+{
+    private readonly IMediator _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+    public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+    {
+        DispatchDomainEvents(eventData.Context).GetAwaiter().GetResult();
+        return base.SavingChanges(eventData, result);
+    }
+
+    public override async ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+    {
+        await DispatchDomainEvents(eventData.Context);
+        return await base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+
+    private async Task DispatchDomainEvents(DbContext? context)
+    {
+        if (context is null)
+        {
+            return;
+        }
+
+        var aggregates = context.ChangeTracker
+                                .Entries<IAggregate>()
+                                .Where(e => e.Entity.DomainEvents.Any())
+                                .Select(e => e.Entity)
+                                .ToList();
+
+        var domainEvents = aggregates.SelectMany(e => e.DomainEvents).ToList();
+
+        aggregates.ForEach(e => e.ClearDomainEvents());
+
+        foreach(var domainEvent in domainEvents)
+        {
+            await _mediator.Publish(domainEvent);
+        }
+                                
+    }
+}
--- EShop.Microservices/src/Services/Ordering/Ordering.Infrastructure/Extensions/DependencyInjection.cs
+++ EShop.Microservices/src/Services/Ordering/Ordering.Infrastructure/Extensions/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Ordering.Infrastructure.Data.Interceptors;
 
@@ -9,9 +10,13 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString("Database");
-        services.AddDbContext<ApplicationDbContext>(options =>
+
+        services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+        services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+        services.AddDbContext<ApplicationDbContext>((sp, options) =>
         {
-            options.AddInterceptors(new AuditableEntityInterceptor());
+            options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
             options.UseSqlServer(connectionString);
         });
 
