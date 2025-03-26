7e78a73a5ed6b39ddb5881122e33a69667783ddc
Davood Hanifi <davood.hanifi@gmail.com>
2025-03-26 23:14:49 +0100

    Added Ordering.Application Dtos.

---
+++ EShop.Microservices/src/Services/Ordering/Ordering.Application/Dtos/OrderDto.cs
@@ -0,0 +1,13 @@
+﻿using Ordering.Domain.Enums;
+
+namespace Ordering.Application.Dtos;
+
+public record OrderDto(
+    Guid Id,
+    Guid CustomerId,
+    string OrderName,
+    AddressDto ShippingAddress,
+    AddressDto BillingAddress,
+    PaymentMethodDto PaymentMethod,
+    OrderStatus Status,
+    List<OrderItemDto> OrderItems);
