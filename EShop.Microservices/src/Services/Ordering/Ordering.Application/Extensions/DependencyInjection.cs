fe60a1279f880be0be459b23052ce7b9d8b0371a
Davood Hanifi <davood.hanifi@gmail.com>
2025-06-12 00:05:40 +0200

    Added logging and validation behavior to Ordering service.

--- EShop.Microservices/src/Services/Ordering/Ordering.Application/Extensions/DependencyInjection.cs
+++ EShop.Microservices/src/Services/Ordering/Ordering.Application/Extensions/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using BuildingBlocks.Behaviors;
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
 namespace Ordering.Application.Extensions;
@@ -7,7 +8,13 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
-        services.AddMediatR(config => config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+        });
+
         return services;
     }
 }
