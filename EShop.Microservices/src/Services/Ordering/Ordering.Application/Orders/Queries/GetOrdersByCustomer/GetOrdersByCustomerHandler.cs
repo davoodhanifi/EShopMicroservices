bc9e63a2fb31910ba5dbb42fde1feb50a225c624
Davood Hanifi <davood.hanifi@gmail.com>
2025-06-12 23:44:41 +0200

    Added GetOrdersByCustomer query handler.

---
+++ EShop.Microservices/src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByCustomer/GetOrdersByCustomerHandler.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Ordering.Application.Orders.Queries.GetOrdersByCustomer;
+
+public class GetOrdersByCustomerHandler (IApplicationDbContext dbContext) : IQueryHandler<GetOrdersByCustomerQuery, GetOrdersByCustomerResult>
+{
+    private readonly IApplicationDbContext _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+
+    public async Task<GetOrdersByCustomerResult> Handle(GetOrdersByCustomerQuery query, CancellationToken cancellationToken)
+    {
+        var orders = await _dbContext.Orders
+           .Include(o => o.OrderItems)
+           .AsNoTracking()
+           .Where(o=> o.CustomerId == CustomerId.Of(query.CustomerId))
+           .OrderBy(o => o.OrderName)
+           .ToListAsync(cancellationToken);
+
+        return new GetOrdersByCustomerResult(orders.ToOrderDtoList());
+    }
+}
