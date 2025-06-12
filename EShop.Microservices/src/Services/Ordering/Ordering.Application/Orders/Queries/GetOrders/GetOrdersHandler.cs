ddbf9d2198bece06092d09a336a9b3f03c14add4
Davood Hanifi <davood.hanifi@gmail.com>
2025-06-13 00:11:18 +0200

    Added GetOrders Handler.

---
+++ EShop.Microservices/src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersHandler.cs
@@ -0,0 +1,29 @@
+﻿using BuildingBlocks.Pagination;
+
+namespace Ordering.Application.Orders.Queries.GetOrders;
+
+public class GetOrdersHandler(IApplicationDbContext dbContext) : IQueryHandler<GetOrdersQuery, GetOrdersResult>
+{
+    private readonly IApplicationDbContext _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+    public async Task<GetOrdersResult> Handle(GetOrdersQuery query, CancellationToken cancellationToken)
+    {
+        var pageIndex = query.PaginationRequest.PageIndex;
+        var pageSize = query.PaginationRequest.PageSize;
+
+        var totalCount = await dbContext.Orders.LongCountAsync(cancellationToken);
+
+        var orders = await _dbContext.Orders
+            .AsNoTracking()
+            .Skip(pageSize * pageIndex)
+            .Take(pageSize)
+            .OrderBy(o => o.OrderName)
+            .ToListAsync(cancellationToken);
+
+        return new GetOrdersResult(
+            new PaginatedResult<OrderDto>(
+                pageIndex,
+                pageSize,
+                totalCount,
+                orders.ToOrderDtoList()));
+    }
+}
