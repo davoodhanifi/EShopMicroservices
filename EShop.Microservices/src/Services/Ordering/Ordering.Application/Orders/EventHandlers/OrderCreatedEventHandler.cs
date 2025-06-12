b49d7a06f61cf957def8787ca0438dc32860535f
Davood Hanifi <davood.hanifi@gmail.com>
2025-06-12 22:43:29 +0200

    Added Domain Events Handlers.

---
+++ EShop.Microservices/src/Services/Ordering/Ordering.Application/Orders/EventHandlers/OrderCreatedEventHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace Ordering.Application.Orders.EventHandlers;
+
+public class OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger) : INotificationHandler<OrderCreatedEvent>
+{
+    private readonly ILogger<OrderCreatedEventHandler> _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+    public async Task Handle(OrderCreatedEvent notification, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation(
+            "Domain Event Handled {DomainEvent}",
+            notification.GetType().Name);
+
+        await Task.CompletedTask;
+    }
+}
---
+++ EShop.Microservices/src/Services/Ordering/Ordering.Application/Orders/EventHandlers/OrderUpdatedEventHandler.cs.cs
@@ -0,0 +1,15 @@
+﻿namespace Ordering.Application.Orders.EventHandlers;
+
+public class OrderUpdatedEventHandler(ILogger<OrderUpdatedEventHandler> logger) : INotificationHandler<OrderUpdatedEvent>
+{
+    private readonly ILogger<OrderUpdatedEventHandler> _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+    public async Task Handle(OrderUpdatedEvent notification, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation(
+            "Domain Event Handled {DomainEvent}",
+            notification.GetType().Name);
+
+        await Task.CompletedTask;
+    }
+}
